6070a0636a81f8366f9575509491e0fcb7921aea
Rick <ricksanchez@labadudapta.com>
2018-01-16 20:54:55 +0300

    Brushed module, but callback does not work

--- public/modules/js/mock/suptest.js
+++ public/modules/js/mock/suptest.js
@@ -1,56 +1,50 @@
 var Http = (function () {
+    function UniversalRequest(method, url, data, callback) {
+        this.method = method;
+        this.url = url;
+        this.data = data;
+        this.callback = callback;
+        var self = this;
+
+        var body = JSON.stringify(data);
+        var xhr = new XMLHttpRequest()
+        xhr.open(method, url);
+        xhr.setRequestHeader('Content-Type', 'application/json')
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                console.log(xhr.responseText);
+            }
+        };
+        xhr.send(body);
+    }
 
     var privateGet = function (url, callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState == 4) {
-                if (xhr.status == 200) {
-                    if (callback && typeof (callback) == "function") {
-                        callback(xhr.responseType, xhr.responseText);
-                    }
-                }
-            }
-        };
-        xhr.send();
+        UniversalRequest('GET', url, callback);
     };
 
-    var get = function(address, cb) {
-        privateGet(address, cb);
+    var privatePost = function (url, data, callback) {
+        UniversalRequest('POST', url, data, callback);
     }
-
-    var privatePost = function(url, data, callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', url);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.onreadystatechange = function() {
-            if(xhr.readyState == 4) {
-                if(xhr.status == 200) {
-                    if(callback && typeof(callback) == "function") {
-                        callback(xhr.responseType ,xhr.responseText);
-                    } 
-                }
-            }
-        };
-        xhr.send(JSON.stringify(data));
-    }
-
-    var post = function(address, info, cb) {
-        privatePost(address, info, cb);
-    }
-
 
     return {
-        get: get,
-        post: post
+        get: privateGet,
+        post: privatePost
     }
 })();
+
+
 
 Http.get("http://localhost:3000/users/getall", function (data) {
     alert("button on get correct " + data);
 });
 
-Http.post("http://localhost:3000/users/register", { name: "Dimoooon", email: "ds@dsj.ru", username: "Dimon", password: "111" }, function (err, data) {
-    alert("button on post correct " + data)
-});
+Http.post("http://localhost:3000/users/register",
+    {
+        name: "Dimoooon",
+        email: "ds@dsj.ru",
+        username: "Dimon",
+        password: "111"
+    }, function (err, data) {
+        alert("button on post correct " + data)
+    });
 
