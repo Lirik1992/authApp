40e94696223c1cea064d858d25005bcaa1385d3b
Rick <ricksanchez@labadudapta.com>
2018-01-21 22:21:56 +0300

    API modification added (jwt)

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ const User = require('../models/user');
 // Register
 router.post('/register', (req, res, next) => {
 
-    var newUser = new User ({
+    var newUser = new User({
         name: req.body.name,
         email: req.body.email,
         username: req.body.username,
@@ -16,10 +16,10 @@ router.post('/register', (req, res, next) => {
     });
 
     User.addUser(newUser, (err, user) => {
-        if(err) {
-            res.json({success: false, msg: 'Failed to register user'});
+        if (err) {
+            res.json({ success: false, msg: 'Failed to register user' });
         } else {
-            res.json({success: true, msg: 'User registered'});
+            res.json({ success: true, msg: 'User registered' });
         }
     });
 });
@@ -30,49 +30,79 @@ router.post('/authenticate', (req, res, next) => {
     const password = req.body.password;
 
     User.getUserByUsername(username, (err, user) => {
-        if(err) throw err;
-        if(!user) {
-            return res.json({success: false, msg:'User not found'});
-        } 
+        if (err) throw err;
+        if (!user) {
+            return res.json({ success: false, msg: 'User not found' });
+        }
         User.comparePassword(password, user.password, (err, isMatch) => {
-            if(err) throw err;
-            if(isMatch) {
-                const token = jwt.sign({data: user}, config.secret, {
-                    expiresIn: 604800 // 1 week
-                });
-
-                res.json({
-                    success: true,
-                    token: 'Bearer ' + token,
-                    user: {
-                        id: user._id,
-                        name: user.name,
-                        username: user.username,
-                        email: user.email
-                    }
+            if (err) throw err;
+            if (isMatch) {
+                const token = jwt.sign({ data: user }, config.secret, { expiresIn: 604800 }, (err, token) => {
+                    res.json({
+                        success: true,
+                        token: 'Bearer ' + token,
+                        user: {
+                            id: user._id,
+                            name: user.name,
+                            username: user.username,
+                            email: user.email
+                        }
+                    });
                 });
             } else {
-                return res.json({success: false, msg: 'Wrong password'});
+                return res.json({ success: false, msg: 'Wrong password' });
             }
         });
     });
 });
 
-// 
-router.get('/profile', passport.authenticate('jwt', {session: false}), (req, res, next) => {
-    res.json({user: req.user});
+// Passport auth
+router.get('/profile', passport.authenticate('jwt', { session: false }), (req, res, next) => {
+    res.json({ user: req.user });
 });
 
 
+// JSON Web Token check
+router.post('/devices/load', verifyToken, (req, res) => {
+    jwt.verify(req.token, config.secret, (err, data) => {
+        if(err) {
+            res.sendStatus(403);
+        } else {
+             res.json({
+                 message: 'Post created',
+                 data,
+             });
+        }
+    });
+});
+
+
+// Verify token MIDDLEWARE 
+function verifyToken(req, res, next) {
+    //Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    if (typeof bearerHeader !== 'undefined') {
+        //Pull the actual token
+        const bearer = bearerHeader.split(' ').splice(1, 1)[0];
+        //Set the token
+        req.token = bearer;
+        // Next MIDDLEWARE
+        next();
+    } else {
+        res.json({ message: "Server responded with the 403 code." })
+    }
+}
+
+
 router.get('/getall', (req, res, next) => {
-    User.find({}, function(err, user) {
+    User.find({}, function (err, user) {
         res.json(user);
     });
 });
 
 // 
 router.get('/validate', (req, res, next) => {
-    res.send('VALIDATE') 
+    res.send('VALIDATE')
 });
 
 module.exports = router;
